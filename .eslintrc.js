cf4b3f635943655bc11aff50853ce8396da67dae
Gustavo C. Oliveira <casagrande.gust@gmail.com>
2020-02-21 20:17:36 -0300

    eslint: disable import/no-named-as-default rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   'rules': {
     'react/jsx-filename-extension': 'off',
     'react/react-in-jsx-scope': 'off',
+    'import/no-named-as-default': 'off',
   },
   'globals': {
     'fetch': false
