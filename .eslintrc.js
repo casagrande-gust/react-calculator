8f6063dc77e9854676a69e428c568f287d6c6e2b
Gustavo C. Oliveira <casagrande.gust@gmail.com>
2020-02-21 19:49:04 -0300

    repository: install and config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'extends': 'airbnb',
+  'parser': 'babel-eslint',
+  'env': {
+    'jest': true,
+  },
+  'rules': {
+    'react/jsx-filename-extension': 'off',
+  },
+  'globals': {
+    'fetch': false
+  }
+}
