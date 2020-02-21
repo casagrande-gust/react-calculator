09349f62a57008506c9088c6e1d3a1eb983d68a7
Gustavo C. Oliveira <casagrande.gust@gmail.com>
2020-02-21 20:11:51 -0300

    DualDisplay: create empty template for component

---
+++ components/DualDisplay.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+/**
+ * DualDisplay Component
+ *
+ * A component that renders two containers,
+ * main and secondary, to display string values.
+ */
+export const DualDisplay = () => (
+  <View />
+);
+
+export default DualDisplay;
