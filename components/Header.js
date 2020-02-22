545746160339b1bf32c5a674aa05f360cd4a8473
Gustavo C. Oliveira <casagrande.gust@gmail.com>
2020-02-22 09:22:42 -0300

    Header: display title input in component

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,21 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 
 /**
  * Header Component
  *
  * Displays the title of the app.
  */
-export const Header = () => (
-  <View />
-);
+export const Header = (props) => {
+  const { title } = props;
+
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  );
+};
 
 export default Header;
 
