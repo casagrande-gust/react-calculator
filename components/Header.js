6b56d85f8b5120d8619a34d88dca14a3d51a507b
Gustavo C. Oliveira <casagrande.gust@gmail.com>
2020-02-22 09:09:31 -0300

    Header: create empty template for component

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+
+/**
+ * Header Component
+ *
+ * Displays the title of the app.
+ */
+export const Header = () => (
+  <View />
+);
+
+export default Header;
