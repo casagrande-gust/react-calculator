b27cede9910adf10ef5271d3f24580bb477ec1c8
Gustavo C. Oliveira <casagrande.gust@gmail.com>
2020-02-22 09:28:36 -0300

    Header: add styling for component

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,23 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Colors from '../constants/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: 90,
+    backgroundColor: Colors.primary,
+    paddingTop: 20,
+  },
+  title: {
+    color: Colors.white,
+    fontSize: 18,
+  },
+});
 
 /**
  * Header Component
@@ -11,8 +28,8 @@ export const Header = (props) => {
   const { title } = props;
 
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
     </View>
   );
 };
