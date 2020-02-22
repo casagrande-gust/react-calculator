1d757b9ff6c26dee0297aae1c5859faed4f31714
Gustavo C. Oliveira <casagrande.gust@gmail.com>
2020-02-22 13:51:54 -0300

    CalcKeyboard: create tests for component

---
+++ components/CalcKeyboard.test.js
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react-native';
+import React from 'react';
+
+import CalcKeyboard from './CalcKeyboard';
+
+describe('CalcKeyboard Component', () => {
+  let testFuncs = {};
+
+  beforeAll(() => {
+    testFuncs = render(<CalcKeyboard />);
+  });
+
+  test.each(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', 'x', '/', 'CLR', '='])(
+    'display button %s',
+    (i) => {
+      expect(testFuncs.getByText(i)).toBeTruthy();
+    },
+  );
+});
