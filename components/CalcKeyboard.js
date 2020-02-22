03f66bb92ae4da52bc3b0c3033efbee86a5c891a
Gustavo C. Oliveira <casagrande.gust@gmail.com>
2020-02-22 13:55:43 -0300

    CalcKeyboard: define PropTypes for component

--- components/CalcKeyboard.js
+++ components/CalcKeyboard.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { View } from 'react-native';
 
@@ -6,3 +7,31 @@ export const CalcKeyboard = () => (
 );
 
 export default CalcKeyboard;
+
+CalcKeyboard.propTypes = {
+  /**
+   * numericBtnFn
+   *
+   * Function executed when a numeric button is pressed.
+   */
+  numericBtnFn: PropTypes.func,
+  /**
+   * operatorBtnFn
+   *
+   * Function executed when an operator button is pressed.
+   */
+  operatorBtnFn: PropTypes.func,
+  /**
+   * clearBtnFn
+   *
+   * Function executed when the "clear" button is pressed.
+   */
+  clearBtnFn: PropTypes.func,
+};
+
+// Default props for CalcKeyboard.
+CalcKeyboard.defaultProps = {
+  numericBtnFn: () => {},
+  operatorBtnFn: () => {},
+  clearBtnFn: () => {},
+};
