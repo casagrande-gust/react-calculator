174f3497b913f140bb080435041257e1a3707d33
Gustavo C. Oliveira <casagrande.gust@gmail.com>
2020-02-22 14:00:21 -0300

    CalcKeyboard: create array to store buttons' properties

--- components/CalcKeyboard.js
+++ components/CalcKeyboard.js
@@ -2,9 +2,40 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { View } from 'react-native';
 
-export const CalcKeyboard = () => (
-  <View />
-);
+export const CalcKeyboard = (props) => {
+  const { numericBtnFn, operatorBtnFn, clearBtnFn } = props;
+
+  const keyboardButtons = [
+    [
+      { title: '7', buttonFn: numericBtnFn },
+      { title: '8', buttonFn: numericBtnFn },
+      { title: '9', buttonFn: numericBtnFn },
+      { title: '+', buttonFn: operatorBtnFn },
+    ],
+    [
+      { title: '4', buttonFn: numericBtnFn },
+      { title: '5', buttonFn: numericBtnFn },
+      { title: '6', buttonFn: numericBtnFn },
+      { title: '-', buttonFn: operatorBtnFn },
+    ],
+    [
+      { title: '1', buttonFn: numericBtnFn },
+      { title: '2', buttonFn: numericBtnFn },
+      { title: '3', buttonFn: numericBtnFn },
+      { title: 'x', buttonFn: operatorBtnFn },
+    ],
+    [
+      { title: 'CLR', buttonFn: clearBtnFn },
+      { title: '0', buttonFn: numericBtnFn },
+      { title: '=', buttonFn: operatorBtnFn },
+      { title: '/', buttonFn: operatorBtnFn },
+    ],
+  ];
+
+  return (
+    <View />
+  );
+};
 
 export default CalcKeyboard;
 
