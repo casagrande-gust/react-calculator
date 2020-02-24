908a0e2380c7cf507c18247c66f533919015cde5
Gustavo C. Oliveira <casagrande.gust@gmail.com>
2020-02-23 22:47:49 -0300

    App: add test to check for correct build/display of input values

--- App.test.js
+++ App.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 import React from 'react';
 
 import App from './App';
@@ -13,5 +13,14 @@ describe('App Component', () => {
 
   it('should display the app header containing the title', () => {
     expect(testFuncs.getAllByText(AppStrings.appTitle).length).toEqual(1);
+  });
+
+  it('should build and display numbers correctly', () => {
+    fireEvent.press(testFuncs.getByText('5'));
+    fireEvent.press(testFuncs.getByText('.'));
+    fireEvent.press(testFuncs.getByText('7'));
+    fireEvent.press(testFuncs.getByText('3'));
+    // Value appears in both containers of DualDisplay
+    expect(testFuncs.getAllByText('5.73').length).toEqual(2);
   });
 });
